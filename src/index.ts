1b59ea85671dbd7941c80d388a288e78b6e0b1d7
D-K-P <dkp.github@pm.me>
2023-02-20 16:28:29 +0000

    Added GitHub issues to Slack scripts etc

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-## ✨ Trigger.dev GitHub Issues to Slack
-
-This repo contains a [GitHub IssueEvent](https://docs.trigger.dev/integrations/apis/github/events/issues) Trigger that will run Trigger a workflow whenever an issue action is performed in a GitHub repository:
-
-```ts
 import { Trigger } from "@trigger.dev/sdk";
 import * as github from "@trigger.dev/github";
 import * as slack from "@trigger.dev/slack";
@@ -54,38 +49,3 @@ new Trigger({
     });
   },
 }).listen();
-```
-
-## ✍️ Customize
-
-1. Make sure and update the `repo` parameter to point to a GitHub repository you manage by setting the `GITHUB_REPOSITORY` environment variable.
-2. Feel free to customize [postMessage](https://docs.trigger.dev/integrations/apis/slack/actions/post-message) call with more data from the [GitHub IssueEvent](https://docs.trigger.dev/integrations/apis/github/events/issues) and change the channel name.
-
-## 💻 Run locally
-
-First, in your terminal of choice, clone the repo and install dependencies:
-
-```sh
-git clone https://github.com/triggerdotdev/github-issues-to-slack.git
-cd github-issues-to-slack
-npm install
-```
-
-Then create a `.env` file at the root of the repository (it's already gitignored) with your development Trigger.dev API Key and GITHUB_REPOSITORY:
-
-```
-TRIGGER_API_KEY=<your api key>
-GITHUB_REPOSITORY=<your github repository> # e.g. triggerdotdev/trigger.dev
-```
-
-And finally you are ready to run the process:
-
-```sh
-npm run dev
-```
-
-You should see a message like the following:
-
-```
-[trigger.dev]  ✨ Connected and listening for events [github-issues-to-slack]
-```
